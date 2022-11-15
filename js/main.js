49de3330cb6a297548ae8ab5ba103a11037009bd
sherry092120 <airkingdom092120@gmail.com>
2022-11-15 21:47:13 +0800

    add 首頁初版、簽署新文件-1

---
+++ js/main.js
@@ -0,0 +1,22 @@
+window.addEventListener('load',function(){
+    const headerApp = Vue.createApp({
+        data(){
+            return{ 
+                title: '',
+                menu: [['邀請他人簽署','disable','#'],['簽署新文件','','./signing.html'],['登入','','#']],
+            }
+        },
+        methods:{
+            open(url){
+                window.location= url;
+            }
+        },
+        mounted(){
+            let title = window.location.pathname;
+            if(title == '/signing.html'){
+                this.title = '簽署新文件';
+            }
+        },
+    })
+    headerApp.mount('#headerApp');
+})
